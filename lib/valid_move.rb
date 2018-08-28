1a22d1217b1dd9fb3e8c03aa4d8a64262f619741
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-28 03:12:50 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,6 +2,11 @@
 def valid_move?(board, index)
   # Returns true if the move is valid (Present on game board, not already filled)
   # Returns false if the move is not valid
+  if present_on_board(index) && !position_taken?(board, index)
+    true
+  else
+    false
+  end
 end
 
 def position_taken?(board, index)
