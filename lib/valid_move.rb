95badd5af7e58849d36ee88ceb8f2e533423c27e
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-28 03:06:39 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,10 @@
 # code your #valid_move? method here
+def valid_move?(board, index)
+  # Returns true if the move is valid (Present on game board, not already filled)
+  # Returns false if the move is not valid
+end
 
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  value_at_index = board[index]
+  if (board[index] == " ") || (board[index])
+end
