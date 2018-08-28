9734b688e11295045f9a0b63d7d5c1f043108319
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-28 03:12:06 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -13,7 +13,7 @@ def position_taken?(board, index)
   end
 end
 
-def valid_position_on_board?(index)
+def present_on_board?(index)
   VALID_INDEXES = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   
   if VALID_INDEXES.include?(index)
